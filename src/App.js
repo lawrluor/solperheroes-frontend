fe33ace77123d7e268466407a5bdd8e8cd9b8a77
Lawrence Luo <luolawrence1@gmail.com>
2021-11-03 00:20:59 -0700

    initial commit with basic styling

--- src/App.js
+++ src/App.js
@@ -5,19 +5,37 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>Solperheroes</p> 
+      </header>
+
+      <main className="App-main">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Solperheroes.
         </p>
         <a
-          className="App-link"
+          className="App-button"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Join Twitter
         </a>
-      </header>
+
+        <a
+          className="App-button"
+          href="https://discord.gg/yDrEzYYje7"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Join Discord
+        </a>
+        
+      </main>
+
+      <footer className="App-footer">
+        Copyright 2021 by Solperheroes NFT
+      </footer>
     </div>
   );
 }
