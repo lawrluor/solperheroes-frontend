30dc421463d6e74734450694c5aefd41a63d3e80
Lawrence Luo <luolawrence1@gmail.com>
2021-11-03 10:49:48 -0700

    staging for deployment

--- src/App.js
+++ src/App.js
@@ -6,18 +6,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h4 className="titleText">SOLPERHEROES</h4> 
+        <h3 className="titleText">SOLPERHEROES</h3> 
       </header>
 
-              {/* <img src={logo} className="App-logo" alt="logo" /> */}
 
       <main className="App-main">
         <div className="mainTextContainer">
-          <span className="bodyText blurbText">
-          </span>
+
+          <img src={"https://github.com/lawrluor/solperheroes-frontend/blob/main/public/img/solperheroes_banner.jpg?raw=true"} className="App-logo" alt="logo" />
 
           <p className="bodyText">
-            SOLPERHEROES is the NFT that is reinventing charity. Exisitng on the Solana blockchain, there are thousands of unique, one-of-a-kind collectibles. Choose your Solperhero and make a difference today!
+            SOLPERHEROES is the NFT that is reinventing charity online. Created on the Solana blockchain, there are thousands of unique, one-of-a-kind collectibles. Choose your Solperhero and make a difference!
           </p>
         </div>
 
